172e0efa99c206b6752f20eaf450a8b6690214ad
Git <mickaeldebalme@MacBook-Pro-de-Mickael.local>
2018-09-19 13:17:23 +0200

    Ajout Ajax

--- js/ajouterSalle.js
+++ js/ajouterSalle.js
@@ -2,8 +2,28 @@
  *
  */
 
-$('#submit_ajouter_salle').click(function(e){
-    e.preventDefault();
+$('#formAjoutSalle').submit(function(e){
+    //e.preventDefault();
 
+    let proprioSalle = $("#proprio_salle").val();
+    let nomSalle = $('#nom_salle').val();
+    let capaciteSalle = $('#capacite_salle').val();
 
-})
+    $.ajax ({
+        type: "GET",
+        url: "ajouterSalle.php",
+        data: {
+            proprioSalle: proprioSalle,
+            nomSalle: nomSalle,
+            capaciteSalle: capaciteSalle
+        },
+        success: function() {
+            console.log(":)");
+        },
+        error: function(data) {
+            console.error(data);
+        }
+    });
+
+    return false;
+});
